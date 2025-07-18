beae0862317126fb62b468218360182e8d80ad67
Suriya2023 <rajputsuraj221221@gmail.com>
2025-07-18 10:06:44 +0530

    minore changes

--- backend/app.js
+++ backend/app.js
@@ -18,6 +18,8 @@ app.use(cors({
     origin: process.env.CLIENT_URL,
     credentials: true,
 }));
+app.use(express.json());
+
 
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
--- backend/src/DataBase/db.js
+++ backend/src/DataBase/db.js
@@ -1,10 +1,14 @@
 import mongoose from "mongoose";
+
 export const connectDB = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGODB_URl);
-        console.log(`MogoDb Connected ${conn.connection.host}`)
-    }
-    catch (e) {
-        console.error("Mongodb Connection Error:", e)
-    }
-}
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`✅ MongoDB Connected: ${conn.connection.host}`);
+  } catch (e) {
+    console.error("❌ MongoDB Connection Error:", e);
+  }
+};
